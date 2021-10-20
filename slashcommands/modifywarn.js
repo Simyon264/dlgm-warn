b0ab16e1dc00f661606b1927c4591f1cbdd9b35e
Simyon <63975668+Simyon264@users.noreply.github.com>
2021-10-20 19:55:01 +0200

    Bestätigung hinzugefügt

--- slashcommands/modifywarn.js
+++ slashcommands/modifywarn.js
@@ -55,10 +55,10 @@ module.exports = {
 
             if (idObj.obj.type == "steam") type = "@steam"
             if (idObj.obj.type == "discord") type = "@discord"
-            
+
             const embed = new discord.MessageEmbed()
-                .setTitle("Verwarnung geändert!")
-                .setDescription(`**Verwarnung für \`${oldName.toString().trim()}\`**\n\n\`${old.toString().trim()}\` wurde zu \`${idObj.obj[change].toString().trim()}\`\n\n`)
+                .setTitle("Verwarnung ändern?")
+                .setDescription(`**Verwarnung für \`${oldName.toString().trim()}\`**\n\n\`${old.toString().trim()}\` wird zu \`${idObj.obj[change].toString().trim()}\`\n\n`)
                 .setColor(0x00AE86)
                 .setFooter(f.localization("slashcommands","getwarn","footer",[idObj.obj.warnid]))
                 .addField(f.localization("slashcommands", "getwarn", "field1t"), f.localization("slashcommands", "getwarn", "field1", [idObj.obj.name.trim()]))
@@ -68,18 +68,67 @@ module.exports = {
             if (idObj.obj.extra) embed.addField(f.localization("slashcommands","getwarn","field6t"), f.localization("slashcommands","getwarn","field6",[idObj.obj.extra.toString().trim()]))
                 .addField(f.localization("slashcommands", "getwarn", "field4t"), f.localization("slashcommands", "getwarn", "field4", [idObj.obj.punkte.toString().trim()]))
                 .addField(f.localization("slashcommands","getwarn","field5t"), f.localization("slashcommands","getwarn","field5",[idObj.obj.by,idObj.obj.byName]))
-            
-            interaction.editReply({
-                embeds: [embed]
-            })
 
-            file = JSON.parse(fs.readFileSync(`./files/warns/${idObj.filename}`))
-            for (let index = 0; index < file.length; index++) {
-                if (file[index].warnid == id) {
-                    file[index] = idObj.obj
-                    fs.writeFileSync(`./files/warns/${idObj.filename}`, JSON.stringify(file))
-                }
+            const button1 = new discord.MessageButton()
+                .setStyle("SUCCESS")
+                .setCustomId("yes")
+                .setEmoji("✔️")
+            const button2 = new discord.MessageButton()
+                .setStyle("DANGER")
+                .setCustomId("no")
+                .setEmoji("✖️")
+            const row = new discord.MessageActionRow()
+                .addComponents(button1)
+                .addComponents(button2)
+
+            const message = await interaction.fetchReply()
+            
+            interaction.editReply({ content: "** **", embeds: [embed], components: [row]})
+            
+            const collector = message.createMessageComponentCollector({ componentType: 'BUTTON', time: 600000 });
+
+
+            let deleted = false
+            collector.on('collect', async i => {
+                if (i.user.id === interaction.user.id) {
+                    i.deferUpdate()
+                    switch (i.customId) {
+                        case "yes":
+                            deleted = true
+                            file = JSON.parse(fs.readFileSync(`./files/warns/${idObj.filename}`))
+                            for (let index = 0; index < file.length; index++) {
+                                if (file[index].warnid == id) {
+                                    file[index] = idObj.obj
+                                    fs.writeFileSync(`./files/warns/${idObj.filename}`, JSON.stringify(file))
+                                }
+                            }
+                            embed.setDescription(`**Verwarnung für \`${oldName.toString().trim()}\`**\n\n\`${old.toString().trim()}\` wurde zu \`${idObj.obj[change].toString().trim()}\`\n\n`)
+                            embed.setTitle("Verwarnung geändert!")
+                            interaction.editReply({ embeds: [embed], components: [] })
+                            collector.stop()
+                            break;
+                        case "no":
+                            collector.stop()
+                            break;
+                    }
+	            } else {
+                    i.deferUpdate()
+                    delMsg(await i.channel.send(f.localization("slashcommands","deletewarn","nobuttons",[i.user.id])), 3000)
+	            }
+            });
+
+            collector.on('end', collected => {
+                if (deleted) return
+                embed.setTitle("Verwarnungsänderung abgebrochen.")
+                interaction.editReply({embeds: [embed], components: [] })
+            });
+
+            async function delMsg(message,time) {
+                await f.sleep(time)
+                message.delete()
             }
+            
+
         } else return interaction.editReply(f.localization("slashcommands", "getwarns", "noperms"))
     }
 }
