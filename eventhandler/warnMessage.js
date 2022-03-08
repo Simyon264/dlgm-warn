9e8d02559cc655a81176d138f6b48cbe3cf4b107
Simyon264 <63975668+Simyon264@users.noreply.github.com>
2022-03-08 20:50:19 +0100

    Another one

--- eventhandler/warnMessage.js
+++ eventhandler/warnMessage.js
@@ -77,7 +77,7 @@ module.exports = {
                 
                 for (let index = 0; index < lastWarn.length; index++) {
                     if (lastWarn[index].extra) {
-                        if (lastWarn.extra.contains("HIDDEN")) {
+                        if (lastWarn.extra.includes("HIDDEN")) {
                             lastWarn.splice(index,1)
                         }
                     }
