0d2c1015fc26b67029e90bfb575963d4c88f91e2
Simyon264 <63975668+Simyon264@users.noreply.github.com>
2022-07-08 18:33:34 +0200

    Stats now get displayed

--- commands/stats.js
+++ commands/stats.js
@@ -19,11 +19,53 @@ module.exports = {
             if (achievements.length !== 0) {
                 achievements = JSON.parse(achievements[0].data)
             }
+
+            const stats = await f.getStats(link.idIngame)
+
             const embed = new discord.MessageEmbed()
                 .setTitle("Deine Stats")
                 .setColor("RANDOM")
                 .setThumbnail(message.author.avatarURL({ dynamic: true }))
                 .addField("Achievements", `${achievements.length} von ${maxAchievements}`);
+
+            const statTranslations = {
+                "id": "ID",
+                "cokesDrunk": "Colas Getrunken:",
+                "scpItemsUsed": "SCP Items Genutzt:",
+                "pinkCandyKills": "Pink Candy Kills:",
+                "assists": "Assists:",
+                "kills": "Kills:",
+                "alldaylightbits": "Alle DayLight Bits",
+                "killDeathRatio": "KD:",
+                "deaths": "Tode:",
+                "bulletsShot": "Kugeln Geschossen:",
+                "scpsKilled": "SCPs Getötet:",
+                "poopedAs173": "173 Tantrums Genutzt:",
+                "sacrificed106": "Bei 106 Geopfert:",
+                "upgraded914": "914 Upgrades:",
+                "doorsOpened": "Türen Geöffnet:",
+                "doorsClosed": "Türen Geschlossen:",
+                "nukesActivated": "Nukes Angemacht:",
+                "optixKilled": "Optix Getötet:",
+                "gotKilledByOptix": "Von Optix Getötet:",
+                "bitsspent": "DayLight Bits Ausgegeben:",
+                "grenadesThrown": "Granaten Geworfen:",
+                "chaosSpawns": "Als Chaos Gespawnt:",
+                "NtfSpawns": "Als NTF Gespawnt:",
+                "damage": "Schaden:",
+                "HidDamage": "HID Schaden:",
+                "itemsUsed": "Items Genutzt:"
+            }
+
+            for (const key in stats) {
+                if (Object.hasOwnProperty.call(stats, key)) {
+                    const element = stats[key];
+                    const current = statTranslations[key]
+                    if (current) {
+                        embed.addField(current, element.toString(), true)
+                    }
+                }
+            }
 
             message.reply({ embeds: [embed] })
         } else {
