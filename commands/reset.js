695823ed0a85aaf3f45d3775b4817dbb739e648e
Simyon264 <63975668+Simyon264@users.noreply.github.com>
2022-07-13 20:34:03 +0200

    Added reset command

---
+++ commands/reset.js
@@ -0,0 +1,23 @@
+const f = require('../functions.js');
+const discord = require('discord.js');
+const fs = require("fs")
+
+module.exports = {
+    name: 'stats',
+    description: f.localization("commands", "stats", "exports").description,
+    category: 'general',
+    modcommand: false,
+    usage: f.localization("commands", "stats", "exports").usage,
+    perms: '',
+    alias: ["stat"],
+    cooldown: 1,
+    run: async function (message, prefix, args, client) {
+        const link = await f.getLink(message.author.id)
+        if (link) {
+            f.resetStats(link.idIngame);
+            message.reply("Deine Daten wurden zurückgesetzt.");
+        } else {
+            message.reply(`Du hast kein Link. Nutze \`${prefix}link\` um dein Discord mit deinem Steam account zu verlinken.`)
+        }
+    }
+}
